ff369d4c457dd5fab5567e2dcb3e7e25a4d2f55c
Juan Carlos Saugar <juank98761@gmail.com>
2025-09-10 12:18:23 +0200

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sonic Waves';
-export const SITE_DESCRIPTION = 'Official website of Sonic Waves - Rock music that moves your soul';
+export const SITE_TITLE = 'Puma\'s Band';
+export const SITE_DESCRIPTION = 'Official website of Puma\'s Band - Rock music that moves your soul';
 export const WIDTH = '7xl';
 
